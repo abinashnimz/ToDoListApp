095f324fc6562291bc09d6c5fdd6f5273b0e6b26
abinashnimz <abinash443steam@gmail.com>
2024-08-29 21:50:20 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ToDo } from "./components/ToDo"
 
 export const App = ()=>{
   return(
-    <div className="bg-violet-950 grid py-4 h-screen">
+    <div className="bg-violet-950 grid py-4 min-h-screen">
       <ToDo/>
     </div>
   )
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -45,7 +45,7 @@ export const ToDo = () => {
 
 
     return (
-        <div className="bg-white place-self-center w-11/12 max-w-xl flex flex-col min-h-[550px] rounded-xl p-7">
+        <div className="bg-white place-self-center w-11/12 max-w-xl flex flex-col min-h-[550px] rounded-xl p-7 mt-5">
             {/* -------Title--------- */}
             <div className="flex items-center mt-7 gap-4">
                 <img src={todo_icon} alt="todo-icon" className="w-8" />
